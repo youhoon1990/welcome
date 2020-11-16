228a6423ad8b2ec80edd4c7ec3dc83bd0a55185d
youhoon1990 <71203728+youhoon1990@users.noreply.github.com>
2020-11-16 17:40:33 +0900

    Add files via upload

---
+++ bonus04점프케이스.java
@@ -0,0 +1,41 @@
+package festival3;
+
+public class bonus04점프케이스 {
+
+	public static void main(String[] args) {
+		System.out.println(jumpCase(4));
+
+	}
+
+//	public static int jumpCase(int input) {
+//		int last1, last2, result = 0;
+//		if (input <= 1) {
+//			return 1;
+//		}
+//		last1 = 1;
+//		last2 = 1;
+//
+//		for (int i = 1; i < input ; i++) {
+//			result = last1 + last2;
+//			last1 = last2;
+//			last2 = result;
+//		}
+//		return result;
+//	}
+	public static int jumpCase(int input) {
+		int num1, num2, result =0;
+		if(input <=1) {
+			return 1;
+		}
+		num1 =1;
+		num2 =1;
+		for(int i=1; i<input; i++ ) {
+			result = num1+num2;
+			num1 = num2;
+			num2 = result;
+		}
+		return result;
+	}
+	
+
+}
